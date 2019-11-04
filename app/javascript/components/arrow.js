cb583428c1a21a18220f7e2631b1631bc62126a3
Baudouin Renie <baudouin@renie.org>
2019-11-04 10:20:29 +0100

    Added arrows JS and changed navbar

--- app/javascript/components/arrow.js
+++ app/javascript/components/arrow.js
@@ -1,34 +1,42 @@
 const initOrangeArrows = () => {
-  const title = document.querySelector('#title');
+  const body = document.querySelector('body');
   const arrow1 = document.querySelector('#arrow1');
   const arrow2 = document.querySelector('#arrow2');
   const arrow3 = document.querySelector('#arrow3');
 
-  title.addEventListener('mouseover', () => {
-    setTimeout(function() {
-      arrow1.classList.add('java');
-    }, 500);
+  body.addEventListener('mouseover', () => {
+    const repeat = () => {
+      setTimeout(function() {
+        arrow1.classList.add('java');
+      }, 500);
 
-    setTimeout(function() {
-      arrow2.classList.add('java');
-    }, 1000);
+      setTimeout(function() {
+        arrow2.classList.add('java');
+      }, 1000);
 
-    setTimeout(function() {
-      arrow3.classList.add('java');
-    }, 1500);
+      setTimeout(function() {
+        arrow3.classList.add('java');
+      }, 1500);
 
-    setTimeout(function() {
-      arrow1.classList.remove('java');
-    }, 2000);
+      setTimeout(function() {
+        arrow1.classList.remove('java');
+      }, 2000);
 
-    setTimeout(function() {
-      arrow2.classList.remove('java');
-    }, 2500);
+      setTimeout(function() {
+        arrow2.classList.remove('java');
+      }, 2500);
 
-    setTimeout(function() {
-      arrow3.classList.remove('java');
-    }, 3000);
-  });
+      setTimeout(function() {
+        arrow3.classList.remove('java');
+      }, 3000);
+
+      setTimeout(function() {
+        repeat();
+      }, 3500);
+    }
+
+    repeat();
+  }, { once: true });
 }
 
 export { initOrangeArrows }
